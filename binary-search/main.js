a8b10e5960c398875f1eb6d4d538a047b63a0a8a
morsko1 <morsko1@yandex.ru>
2020-05-27 00:30:03 +0300

    refactored binary search to class

--- binary-search/main.js
+++ binary-search/main.js
@@ -1,133 +1,139 @@
-const container = document.getElementById('container');
-
-const arr = Array.from(new Array(20), (x, i) => i + 1);
-
-let state = {
-    step: 0,
-    current: null,
-    processed: [],
-    start: 0,
-    end: arr.length - 1,
-    inProcess: false,
-    found: null
-};
-
-function start (e) {
-    e.preventDefault();
-    if (state.inProcess) {
-        return;
+class BinarySearch {
+    constructor(array) {
+        this.array = array;
+        this.container = document.getElementById('container');
+        this.state = {
+            step: 0,
+            current: null,
+            processed: [],
+            start: 0,
+            end: array.length - 1,
+            inProcess: false,
+            found: null
+        };
+        this.init();
     }
-    state = {
-        step: 0,
-        current: null,
-        processed: [],
-        start: 0,
-        end: arr.length - 1,
-        inProcess: true,
-        found: null
-    };
-    let item = parseInt(e.target[0].value);
-    binarySearchAsync(arr, item);
-    return false;
-}
 
-async function binarySearchAsync (arr, item) {
-    let middle;
-    state.inProcess = true;
-    const setPointer = () => new Promise((resolve) => {
-        setTimeout(() => {
-            middle = Math.floor((state.start + state.end) / 2);
-            state.current = middle;
-            console.log('setPointer');
-            render();
-            resolve();
-        }, 1000);
-    });
+    start(e) {
+        e.preventDefault();
+        if (this.state.inProcess) {
+            return;
+        }
+        this.state = {
+            step: 0,
+            current: null,
+            processed: [],
+            start: 0,
+            end: this.array.length - 1,
+            inProcess: true,
+            found: null
+        };
+        const item = parseInt(e.target[0].value);
+        this.binarySearchAsync(this.array, item);
+        return false;
+    }
 
-    const trimStart = () => new Promise((resolve) => {
-        setTimeout(() => {
-            state.start = middle + 1;
-            render();
-            resolve();
-        }, 1000);
-    });
-
-    const trimEnd = () => new Promise((resolve) => {
-        setTimeout(() => {
-            state.end = middle - 1;
-            render();
-            resolve();
-        }, 1000);
-    });
-
-    const checkItem = () => new Promise((resolve) => {
-        setTimeout(async () => {
-            if (item === arr[middle]) {
-                console.log('found, index is: ', middle);
-                console.log('iteration № ', state.step);
-                state.inProcess = false;
-                state.found = middle;
-                render();
-                resolve(arr[middle]);
-            } else if (item < arr[middle]) {
-                console.log('less');
-                state.processed.push(middle);
-                render();
-                await trimEnd();
+    async binarySearchAsync(arr, item) {
+        let middle;
+        this.state.inProcess = true;
+        const setPointer = () => new Promise((resolve) => {
+            setTimeout(() => {
+                middle = Math.floor((this.state.start + this.state.end) / 2);
+                this.state.current = middle;
+                console.log('setPointer');
+                this.render();
                 resolve();
-            } else if (item > arr[middle]) {
-                console.log('bigger');
-                state.processed.push(middle);
-                render();
-                await trimStart();
+            }, 1000);
+        });
+
+        const trimStart = () => new Promise((resolve) => {
+            setTimeout(() => {
+                this.state.start = middle + 1;
+                this.render();
                 resolve();
+            }, 1000);
+        });
+
+        const trimEnd = () => new Promise((resolve) => {
+            setTimeout(() => {
+                this.state.end = middle - 1;
+                this.render();
+                resolve();
+            }, 1000);
+        });
+
+        const checkItem = () => new Promise((resolve) => {
+            setTimeout(async () => {
+                if (item === this.array[middle]) {
+                    console.log('found, index is: ', middle);
+                    console.log('iteration № ', this.state.step);
+                    this.state.inProcess = false;
+                    this.state.found = middle;
+                    this.render();
+                    resolve(this.array[middle]);
+                } else if (item < this.array[middle]) {
+                    console.log('less');
+                    this.state.processed.push(middle);
+                    this.render();
+                    await trimEnd();
+                    resolve();
+                } else if (item > this.array[middle]) {
+                    console.log('bigger');
+                    this.state.processed.push(middle);
+                    this.render();
+                    await trimStart();
+                    resolve();
+                }
+                if (this.state.start > this.state.end) {
+                    this.state.inProcess = false;
+                    console.log('not found');
+                    console.log('iteration № ', this.state.step);
+                    this.render();
+                    resolve(undefined);
+                }
+            }, 1000);
+        });
+
+        const doStep = () => new Promise(async (resolve) => {
+            this.state.step++;
+            await setPointer();
+            await checkItem();
+            resolve();
+        });
+
+        this.render();
+
+        while (this.state.inProcess) {
+            await doStep();
+        }
+    }
+
+    render() {
+        this.container.innerHTML = '';
+        for (let i = 0; i < this.array.length; i++) {
+            const elem = document.createElement('div');
+            elem.innerHTML = this.array[i];
+            elem.classList.add('item');
+            if (i < this.state.start || i > this.state.end) {
+                elem.classList.add('disabled');
             }
-            if (state.start > state.end) {
-                state.inProcess = false;
-                console.log('not found');
-                console.log('iteration № ', state.step);
-                render();
-                resolve(undefined);
+            if (i === this.state.current) {
+                elem.classList.add('current');
             }
-        }, 1000);
-    });
+            if (this.state.processed.includes(i)) {
+                elem.classList.add('processed');
+            }
+            if (i === this.state.found) {
+                elem.classList.add('found');
+            }
 
-    const doStep = () => new Promise(async (resolve) => {
-        state.step++;
-        await setPointer();
-        await checkItem();
-        resolve();
-    });
+            this.container.appendChild(elem);
+        }
+    }
 
-    render();
-
-    while (state.inProcess) {
-        await doStep();
+    init() {
+        this.render();
     }
 }
 
-function render () {
-    container.innerHTML = '';
-    for (let i = 0; i < arr.length; i++) {
-        const elem = document.createElement('div');
-        elem.innerHTML = arr[i];
-        elem.classList.add('item');
-        if (i < state.start || i > state.end) {
-            elem.classList.add('disabled');
-        }
-        if (i === state.current) {
-            elem.classList.add('current');
-        }
-        if (state.processed.includes(i)) {
-            elem.classList.add('processed');
-        }
-        
-        if (i === state.found) {
-            elem.classList.add('found');
-        }
-
-        container.appendChild(elem);
-    }
-}
-
-render();
+const binarySearch = new BinarySearch(Array.from(new Array(20), (x, i) => i + 1));
